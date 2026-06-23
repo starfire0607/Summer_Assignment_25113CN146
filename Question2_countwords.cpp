15c0f89409c1cc5365ea91d7ee99cbaa83f85920
starfire0607 <itachiuchiha11511@gmail.com>
2026-06-23 13:30:42 +0530

    initial commit

---
+++ Question2_countwords.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+
+    int count = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+        if ((i == 0 && str[i] != ' ') ||
+            (str[i] != ' ' && str[i - 1] == ' ')) {
+            count++;
+        }
+    }
+
+    cout << "Number of words = " << count;
+
+    return 0;
+}
